35e2dfc53d3f0d0123bffd8ac4f2c7a6935d29fc
rahulrana1241 <109511854+rahulrana1241@users.noreply.github.com>
2022-07-20 14:47:26 +0530

    Add files via upload

---
+++ areaofpython.py
@@ -0,0 +1,9 @@
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
+
+#reverse a list
+a=[5,"ram",10,"ravi",3]
+a.reverse()
+print(a)
